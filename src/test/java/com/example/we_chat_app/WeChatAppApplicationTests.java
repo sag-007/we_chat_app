7b81133b73c5cbef9e15ea5f772fe75744cbe3b1
Sagar C A <aswathg17@gmail.com>
2020-07-05 00:41:10 +0530

    Initial commit

---
+++ src/test/java/com/example/we_chat_app/WeChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.we_chat_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
